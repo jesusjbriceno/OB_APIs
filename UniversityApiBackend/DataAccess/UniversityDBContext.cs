911d708d194e37b4583b4458d7aa9d03724f468e
Jesús Briceño <jesusj.briceno@gmail.com>
2023-02-20 17:16:18 +0100

    First commit

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) { }
+
+        // Add DbSets
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(200)]
+        public string ShortDescription { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Target { get; set; } = string.Empty;
+        public string Objectives { get; set; } = string.Empty;
+        public string Requirements { get; set; } = string.Empty;
+        public EnumLevel Level { get; set; }
+    }
+
+    public enum EnumLevel
+    {
+        Basic=0,
+        Medium=1,
+        Advance=2
+    }
+}
