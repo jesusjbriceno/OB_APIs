bcea508a55e763d2c708847630eb63e0e3358864
Jesús Briceño <jesusj.briceno@gmail.com>
2023-03-07 13:49:23 +0100

    Configuracion Serilog con EF

--- UniversityApiBackend/DataAccess/UniversityDBContext.cs
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -5,7 +5,13 @@ namespace UniversityApiBackend.DataAccess
 {
     public class UniversityDBContext : DbContext
     {
-        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) { }
+        private readonly ILoggerFactory _loggerFactory;
+
+
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options, ILoggerFactory loggerFactory) : base(options) 
+        { 
+            _loggerFactory = loggerFactory;
+        }
 
         // Add DbSets
         public DbSet<User>? Users { get; set; }
@@ -13,6 +19,20 @@ namespace UniversityApiBackend.DataAccess
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Chapter>? Indexes { get; set; }
         public DbSet<Student>? Students { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configure Logger to EF
+            var logger = _loggerFactory.CreateLogger<UniversityDBContext>();
+            // optionsBuilder.LogTo(options => logger.Log(LogLevel.Information, options, new[] { DbLoggerCategory.Database.Name }));
+            // optionsBuilder.EnableSensitiveDataLogging();
+
+            optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }), LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
     }
 
 }
